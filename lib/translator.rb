629b6467ed6afa3316a3c2bd90a8094090180de5
H B <phajib@gmail.com>
2019-02-04 10:13:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 def get_japanese_emoticon(path, emoticon)
   emoticon_hash = load_library(path)
   answer = emoticon_hash["get_emoticon"][emoticon]
-  answer ? answer : return "Sorry, that emoticon was not found"
+  answer ? answer : return "Sorry, that emoticon was not found" end
   #if answer
   #  answer
   #else
