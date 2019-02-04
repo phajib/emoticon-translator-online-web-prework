eef712645b5b76cf990d8eb1a81ae2790a72ed16
H B <phajib@gmail.com>
2019-02-04 09:51:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,8 +2,7 @@ require 'yaml'
 
 def load_library(yml)
   yml = YAML.load_file("./lib/emoticons.yml")
-  yml
-
+  #yml
   emoticons = {"get_meaning"=>{}, "get_emoticon"=>{}}
   yml.each do |meaning, translation|
     eng, jap = translation
@@ -14,7 +13,7 @@ def load_library(yml)
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
